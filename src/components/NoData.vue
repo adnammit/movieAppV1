1a4788b6c13b385667fa4f127f05578a8b45c29b
aryman <amandaryman@gmail.com>
2020-11-05 14:56:19 -0800

    Make noData more fun

--- src/components/NoData.vue
+++ src/components/NoData.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="hero">
 		<div>
-			<h1 class="display-3">Nothin in your collection</h1>
+			<h1 class="display-3">Nothin' in your collection</h1>
+			<font-awesome-icon icon="kiwi-bird" size="6x" style="margin: 20px;" />
+			<font-awesome-icon icon="seedling" size="2x" style="margin: 20px;" />
 			<p class="lead">Use the search field up top to start adding</p>
 		</div>
 	</div>
